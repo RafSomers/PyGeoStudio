d46398452985a8ec3d8f3d75d44db3a5913b5e01
RafSomers <rafsomers2@gmail.com>
2025-09-04 13:25:26 +0200

    Updated test-script 'analysis_advanced.py' to integrate working code to modify boundary conditions in GeoStudio (method is similar to materials).

--- examples/analysis/analysis_advanced.py
+++ examples/analysis/analysis_advanced.py
@@ -31,18 +31,27 @@ geofile.showMaterials()
 mat_1 = geofile.getMaterialByID(1)
 print(mat_1)
 
+# Show boundary conditions
+geofile.showBoundaryConditions()
+
 # Materials & Hydraulic boundary conditions are linked to geometry in object 'analysis.data["Context"]'
 context = analysis.data["Context"]
 geometry = geofile.getGeometryByID(1)
-if "GeometryUsesMaterials" not in context.data:
-  print("No materials assigned to regions. Only geometry is shown.")
-else:
-  mat_ids_used_per_region = context.data["GeometryUsesMaterials"]
-  for reg, mat_id in mat_ids_used_per_region.items():
-      print(reg)
-      print("Material:", mat_id)
-      pts = geometry["Regions"][reg][0]
-      print("Point numbers:", pts)
+mat_ids_used_per_region = context.data["GeometryUsesMaterials"]
+for reg, mat_id in mat_ids_used_per_region.items():
+    print(reg)
+    pts = geometry["Regions"][reg][0]
+    print("Point numbers:", pts)
+    print("Material:", mat_id)
+
+hydBC_ids_used_per_line = context.data["GeometryUsesHydraulicBCs"]
+for ln, hydBC_id in hydBC_ids_used_per_line.items():
+    print(ln)
+    pt_ids = geometry["Lines"][int(ln.split('-')[1])-1]
+    pt_nums = [int(id + 1) for id in pt_ids]
+    print("Point numbers:", pt_nums)
+    print("HydraulicBC:", hydBC_id)
+
 
 # Show problem with regions colored based on mat_id (uses 'draw' from Geometry.py)
 analysis.showProblem()
@@ -50,11 +59,17 @@ analysis.showProblem()
 # Change material of inner dike (= Region-4) to Clay (Drained) with ID = 3
 mat_ids_used_per_region['Regions-4'] = 3
 print(mat_ids_used_per_region)
+
+# Change boundary condition of landside (= Lines-4) to Drainage with ID = 5
+hydBC_ids_used_per_line['Lines-4'] = 5
+print(hydBC_ids_used_per_line)
+
+# Show problem again with regions colored based on mat_id (uses 'draw' from Geometry.py)
 analysis.showProblem()
 
 # %%
 # Write modified study under new file:
-out_file = "examples/GeoStudio_files/Test-adv-1-SEEP-model_Schelde_T4000.gsz"
+out_file = "examples/GeoStudio_files/Test2_mat_hydbc_out.gsz"
 geofile.saveAs(main_path+"/"+out_file)
 
 print("End script!")
